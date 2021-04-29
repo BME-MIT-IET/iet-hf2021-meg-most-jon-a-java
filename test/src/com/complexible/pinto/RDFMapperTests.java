65c7c4ec0163e87286d60c5998ccd2bb269f033d
mherczku <61518890+mherczku@users.noreply.github.com>
2021-04-29 11:28:33 +0200

    update gradle properties

--- test/src/com/complexible/pinto/RDFMapperTests.java
+++ test/src/com/complexible/pinto/RDFMapperTests.java
@@ -57,8 +57,7 @@ import java.util.SortedSet;
 import java.util.TimeZone;
 import java.util.UUID;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * <p></p>
